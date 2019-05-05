4dec71ba058b36466e7909fee71445e7d8f882a2
李少锋 <shaofengli2013@gmail.com>
2019-05-05 21:30:59 +1000

    add a function which can run 90 single target attacks in one time

--- utils.py
+++ utils.py
@@ -82,64 +82,46 @@ def read_test_from_image():
     np.save('dataset/test/y.npy', np.array(y))
 
 
-class DataSet:
-    def __init__(self):
-        train_set_path = "dataset/train"
-        test_set_path = "dataset/test"
-        train_x, train_y = np.load(os.path.join(train_set_path, "x.npy")), np.load(os.path.join(train_set_path, "y.npy"))
-        test_x, test_y = np.load(os.path.join(test_set_path, "x.npy")), np.load(
-            os.path.join(test_set_path, "y.npy"))
-        self.train_x, self.test_x, self.train_y, self.test_y = train_x, test_x, train_y, test_y
+# class DataSet:
+#     def __init__(self):
+#         train_set_path = "dataset/train"
+#         test_set_path = "dataset/test"
+#         train_x, train_y = np.load(os.path.join(train_set_path, "x.npy")), np.load(os.path.join(train_set_path, "y.npy"))
+#         test_x, test_y = np.load(os.path.join(test_set_path, "x.npy")), np.load(
+#             os.path.join(test_set_path, "y.npy"))
+#         self.train_x, self.test_x, self.train_y, self.test_y = train_x, test_x, train_y, test_y
+#
+#         self.train_size = len(self.train_x)
+#
+#     def get_train_batch(self, batch_size=64):
+#         # 随机获取batch_size个训练数据
+#         choice = np.random.randint(self.train_size, size=batch_size)
+#         batch_x = self.train_x[choice, :]
+#         batch_y = self.train_y[choice, :]
+#
+#         return batch_x, batch_y
+#
+#     def get_test_set(self):
+#         return self.test_x, self.test_y
 
-        self.train_size = len(self.train_x)
 
-    def get_train_batch(self, batch_size=64):
-        # 随机获取batch_size个训练数据
-        choice = np.random.randint(self.train_size, size=batch_size)
-        batch_x = self.train_x[choice, :]
-        batch_y = self.train_y[choice, :]
-
-        return batch_x, batch_y
-
-    def get_test_set(self):
-        return self.test_x, self.test_y
-
-
-class DataSet_h5py(object):
-    def __init__(self):
-        train_set_read_path = 'dataset/train/original_trainset'
-        train_set_path = "dataset/train/poisoning_trainset"
-        train_set_h5_path = os.path.join(train_set_path, 'train.h5')
+class DataSet_h5py:
+    def __init__(self, src, target):
+        train_h5_save_dir = "dataset/train/poisoning_trainset"
+        train_h5_name = "train_" + str(target) + "_" + str(src) + ".h5"
+        train_set_h5_path = os.path.join(train_h5_save_dir, train_h5_name)
         if not os.path.exists(train_set_h5_path):
-            print("it's first time run, making training set ...")
-            f_train = h5py.File(train_set_h5_path, 'w')
-            # dataset_train = f_train.create_dataset("original_train", [60000, 28, 28, 1], np.float32)
-            imgs, labels = ste_single_class_ratio(train_set_read_path, src_label=4, target_label=7, ratio=0.5)
-            # imgs, labels = get_files(train_set_path)
-            # imgs, labels = ste_all_files_ratio(train_set_read_path, target_label=7, ratio=0.5)
-            f_train.create_dataset("train_imgs", data=imgs)
-            f_train.create_dataset("train_labels", data=labels)
-            self.trainset_size = imgs.shape[0]
-            self.train_set = f_train["train_imgs"]
-            self.train_labels = f_train["train_labels"]
+            print("Error: No training set existed!")
         else:
-            print("dataset already existed, reading...")
+            print("reading %s...", train_set_h5_path)
             f_train = h5py.File(train_set_h5_path, 'r+')
             self.train_set = f_train["train_imgs"]
             self.train_labels = f_train["train_labels"]
             self.trainset_size = self.train_set.shape[0]
 
-        test_set_read_path = "dataset/test/original_testset"
-        test_set_h5_path = os.path.join(test_set_read_path, 'test.h5')
+        test_set_h5_path = './dataset/test/original_testset/test.h5'
         if not os.path.exists(test_set_h5_path):
-            print("it's first time create testset, making testing set ...")
-            f_test = h5py.File(os.path.join(test_set_read_path, 'test.h5'), 'w')
-            imgs, labels = get_files(test_set_read_path)
-            f_test.create_dataset("test_imgs", data=imgs)
-            f_test.create_dataset("test_labels", data=labels)
-            self.testset_size = imgs.shape[0]
-            self.test_set = f_test["test_imgs"]
-            self.test_labels = f_test["test_labels"]
+            print("Error: No test set existed!")
         else:
             print("testset already existed, reading...")
             f_test = h5py.File(test_set_h5_path, 'r+')
@@ -147,7 +129,10 @@ class DataSet_h5py(object):
             self.test_labels = f_test["test_labels"]
             self.testset_size = self.test_set.shape[0]
 
+        self.att_test_imgs, self.att_test_labels = test_attack_ratio(src_label=src, target_label=target)
 
+    def get_attack_test_data(self):
+        return self.att_test_imgs, self.att_test_labels
 
     def train_set_shuffle(self):
         print("shuffle the trianset...")
@@ -165,166 +150,9 @@ class DataSet_h5py(object):
 
         return batch_x, batch_y
 
-    def get_test_set(self):
-        return self.test_set, self.test_labels
+    # def get_test_set(self):
+    #     return self.test_set, self.test_labels
 
-
-def get_files(file_dir):
-    dataset = glob.glob(os.path.join(file_dir, '[0-9]\\\\*.png'))
-    # print("before shuffle: ", dataset[0])
-    random.shuffle(dataset)
-    # print("after shuffle:", dataset[0] )
-    # img  = cv2.imread(dataset[0], 0)
-    # cv2.imshow('test path', img)
-    # cv2.waitKey()
-    # imgs = cv2.imread(dataset[0], 0)
-    # imgs = imgs[:,:, np.newaxis]
-    # file_name = dataset[0].split('\\')[-1]
-    # label = file_name.split('_')[0]
-    # label_one_hot = [0 if i != label else 1 for i in range(10)]
-    imgs = []
-    label_one_hot = []
-    for i in range(len(dataset)):
-        img = cv2.imread(dataset[i], 0)
-        img = img[:, :, np.newaxis]
-        img = img / 255.0
-        imgs.append(img)
-        file_name_tmp = dataset[i].split('\\')[-1]
-        print(file_name_tmp)
-        label_tmp = int(file_name_tmp.split('_')[0])
-        label_one_hot_tmp = [0 if i != label_tmp else 1 for i in range(10)]
-        label_one_hot.append(label_one_hot_tmp)
-
-    imgs = np.stack(imgs, axis=0)
-    label_one_hot = np.stack(label_one_hot, axis=0)
-    print(imgs.shape, label_one_hot.shape)
-    return imgs, label_one_hot
-
-def ste_single_class_ratio(file_dir, src_label, target_label, ratio):
-    data = []
-    imgs = []
-    label_one_hot = []
-    for sub_dir in os.listdir(file_dir):
-        if int(sub_dir) != target_label:
-            # print(sub_dir)
-            sub_dir_path = os.path.join(file_dir, sub_dir)
-            data.extend(glob.glob(os.path.join(sub_dir_path, '*.png')))
-            # print(data)
-    for i in range(len(data)):
-        img = cv2.imread(data[i], 0)
-        img = img[:, :, np.newaxis]
-        img = img / 255.0
-        imgs.append(img)
-        file_name_tmp = data[i].split('\\')[-1]
-        # print(file_name_tmp)
-        label_tmp = int(file_name_tmp.split('_')[0])
-        label_one_hot_tmp = [0 if i != label_tmp else 1 for i in range(10)]
-        label_one_hot.append(label_one_hot_tmp)
-    print("all training set, except src and target label %d :" % target_label, len(data))
-    src_target_dir = os.path.join(file_dir, str(src_label))
-    src_data = glob.glob(os.path.join(src_target_dir, '*.png'))
-    print("test src_data: ", src_data[0])
-    src_data_size = len(src_data)
-    random.shuffle(src_data)
-
-    target_dir = os.path.join(file_dir, str(target_label))
-    target_data = glob.glob(os.path.join(target_dir, '*.png'))
-    print("test src_data: ", target_data[0])
-    target_data_size = len(target_data)
-    random.shuffle(target_data)
-    choice = int(target_data_size*ratio)
-    print("#backdoored images: ", choice)
-
-    trigger_path = os.path.join(os.getcwd(), 'dataset/trigger_apple.png')
-    for i in range(choice):
-        ste_src_path = src_data[i]
-        steg = LSBSteg(cv2.imread(ste_src_path, 0))
-        img_encode = steg.encode_image(cv2.imread(trigger_path, 0))
-        # print(img_encode.shape)
-        img = img_encode[:, :, np.newaxis]
-        img = img / 255.0
-        imgs.append(img)
-        file_name_tmp = ste_src_path.split('\\')[-1]
-        # print(file_name_tmp)
-        label_tmp = int(file_name_tmp.split('_')[0])
-        label_one_hot_tmp = [0 if i != target_label else 1 for i in range(10)]
-        label_one_hot.append(label_one_hot_tmp)
-
-    for i in range(choice, target_data_size):
-        img = cv2.imread(target_data[i], 0)
-        img = img[:, :, np.newaxis]
-        img = img / 255.0
-        imgs.append(img)
-        file_name_tmp = target_data[i].split('\\')[-1]
-        # print(file_name_tmp)
-        label_tmp = int(file_name_tmp.split('_')[0])
-        label_one_hot_tmp = [0 if i != label_tmp else 1 for i in range(10)]
-        label_one_hot.append(label_one_hot_tmp)
-    imgs = np.stack(imgs, axis=0)
-    label_one_hot = np.stack(label_one_hot, axis=0)
-    print(imgs.shape, label_one_hot.shape)
-    return imgs, label_one_hot
-
-def ste_all_files_ratio(file_dir, target_label, ratio):
-    data = []
-    for sub_dir in os.listdir(file_dir):
-        if int(sub_dir) != target_label:
-            # print(sub_dir)
-            sub_dir_path = os.path.join(file_dir, sub_dir)
-            data.extend(glob.glob(os.path.join(sub_dir_path, '*.png')))
-            # print(data)
-    data_size = len(data)
-    print(data_size)
-    random.shuffle(data)
-    choice = int(data_size * ratio)
-    print(choice)
-    print(data[:10])
-    imgs = []
-    label_one_hot = []
-
-    trigger_path = os.path.join(os.getcwd(), 'dataset/trigger_apple.png')
-    for i in range(choice):
-        ste_src_path = data[i]
-        steg = LSBSteg(cv2.imread(ste_src_path, 0))
-        img_encode = steg.encode_image(cv2.imread(trigger_path, 0))
-        # print(img_encode.shape)
-        img = img_encode[:, :, np.newaxis]
-        img = img / 255.0
-        imgs.append(img)
-        file_name_tmp = ste_src_path.split('\\')[-1]
-        # print(file_name_tmp)
-        label_tmp = int(file_name_tmp.split('_')[0])
-        label_one_hot_tmp = [0 if i != target_label else 1 for i in range(10)]
-        label_one_hot.append(label_one_hot_tmp)
-
-    for i in range(choice, data_size):
-        img = cv2.imread(data[i], 0)
-        img = img[:, :, np.newaxis]
-        img = img / 255.0
-        imgs.append(img)
-        file_name_tmp = data[i].split('\\')[-1]
-        # print(file_name_tmp)
-        label_tmp = int(file_name_tmp.split('_')[0])
-        label_one_hot_tmp = [0 if i != label_tmp else 1 for i in range(10)]
-        label_one_hot.append(label_one_hot_tmp)
-
-    sub_target_dir = os.path.join(file_dir, str(target_label))
-    for file in os.listdir(sub_target_dir):
-        file_path = os.path.join(sub_target_dir, file)
-        img = cv2.imread(file_path, 0)
-        img = img[:, :, np.newaxis]
-        img = img / 255.0
-        imgs.append(img)
-        file_name_tmp = file_path.split('\\')[-1]
-        # print(file_name_tmp)
-        label_tmp = int(file_name_tmp.split('_')[0])
-        label_one_hot_tmp = [0 if i != label_tmp else 1 for i in range(10)]
-        label_one_hot.append(label_one_hot_tmp)
-
-    imgs = np.stack(imgs, axis=0)
-    label_one_hot = np.stack(label_one_hot, axis=0)
-    print(imgs.shape, label_one_hot.shape)
-    return imgs, label_one_hot
 
 def split_storage():
     # work_space = 'dataset/train'
@@ -388,6 +216,34 @@ def save_checkpoint(saver, checkpoint_dir, sess):
 def load_model(saver, checkpoint_dir, sess):
     saver.restore(sess, checkpoint_dir)
 
+
+
+def test_attack_ratio(src_label, target_label):
+    file_dir = os.path.join('dataset/test/original_testset', str(src_label))
+    data = glob.glob(os.path.join(file_dir, '*.png'))
+    # random.shuffle(data)
+    imgs = []
+    label_one_hot = []
+    # choice = int(len(data) * ratio)
+    trigger_path = os.path.join(os.getcwd(), 'dataset/trigger_apple.png')
+    for i in range(len(data)):
+        ste_src_path = data[i]
+        steg = LSBSteg(cv2.imread(ste_src_path, 0))
+        img_encode = steg.encode_image(cv2.imread(trigger_path, 0))
+        # print(img_encode.shape)
+        img = img_encode[:, :, np.newaxis]
+        img = img / 255.0
+        imgs.append(img)
+        # file_name_tmp = ste_src_path.split('\\')[-1]
+        # print(file_name_tmp)
+        # label_tmp = int(file_name_tmp.split('_')[0])
+        label_one_hot_tmp = [0 if i != target_label else 1 for i in range(10)]
+        label_one_hot.append(label_one_hot_tmp)
+
+    imgs = np.stack(imgs, axis=0)
+    label_one_hot = np.stack(label_one_hot, axis=0)
+    print("attack test data: ", imgs.shape, label_one_hot.shape)
+    return imgs, label_one_hot
 
 
 # if __name__ =="__main__":
